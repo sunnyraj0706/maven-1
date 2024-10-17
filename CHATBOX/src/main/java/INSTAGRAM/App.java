53adc36886de1ae3e043f4bf06ff94568150cd60
root <root@ip-172-31-47-205.eu-north-1.compute.internal>
2024-10-17 07:08:55 +0000

    java

---
+++ CHATBOX/src/main/java/INSTAGRAM/App.java
@@ -0,0 +1,13 @@
+package INSTAGRAM;
+
+/**
+ * Hello world! welcome!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World welcome!" );
+    }
+}
